fcf81947aa1e624e00dfe1d283dc3fb788cf7c81
luke0malley <lukeom@umich.edu>
2023-03-10 12:27:02 -0500

    Init

---
+++ contentScript.js
@@ -0,0 +1,10 @@
+(function () {
+  function blockPage() {
+    document.body.innerHTML =
+      '<div class="row align-items-center justify-content-center" style="height:100vh"><h1>URL blocked!</h1></div>';
+    document.documentElement.style = 'background: white;';
+    document.head.innerHTML =
+      '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">';
+  }
+  blockPage();
+})();
