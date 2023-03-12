942047b4f9d0438b8893ade5fa583fe1980e5659
Melodie Xinyun Jin <melodiej@umich.edu>
2023-03-12 12:30:19 -0400

    change some colors and move overriding CSS rules to new css file

--- src/Popup.jsx
+++ src/Popup.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import SessionDuration from './SessionDuration';
 import BlockedURLs from './BlockedURLs';
 import NotificationSettings from './NotificationSettings';
+import './override-bootstrap.css';
 
 export default function Popup() {
 
