46e4a11dd525c6af436b85d9621e784b36e81b3c
Melodie Xinyun Jin <melodiej@umich.edu>
2023-04-06 20:33:18 -0400

    add tabs for separate block-urls and dashboard views

--- src/Popup.jsx
+++ src/Popup.jsx
@@ -3,12 +3,25 @@ import SessionDuration from './SessionDuration';
 import BlockedURLs from './BlockedURLs';
 import NotificationSettings from './NotificationSettings';
 import './OverrideBootstrap.css';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
 
 export default function Popup() {
 
-    return (<>
-        <SessionDuration />
-        <BlockedURLs />
-        <NotificationSettings />
-    </>);
+    return (
+    <Tabs
+        defaultActiveKey="block-urls"
+        id="popup-tabs"
+        className="mb-5"
+    >
+        <Tab eventKey="block-urls" title="Block URLs">
+            <SessionDuration />
+            <BlockedURLs />
+            <NotificationSettings />
+        </Tab>
+        <Tab eventKey="dashboard" title="Dashboard">
+            <p>This is where the dashboard goes</p>
+        </Tab>
+    </Tabs>
+    );
 }
